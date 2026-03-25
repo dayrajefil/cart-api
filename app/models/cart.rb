1aaab664f513c0cdf887de87bde059b7d47a168a
dayrajefil <dayrajefil@gmail.com>
2026-03-25 20:12:37 -0300

    feat: implement Cart and CartItem models with associations and business logic

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,5 +1,18 @@
 class Cart < ApplicationRecord
-  validates_numericality_of :total_price, greater_than_or_equal_to: 0
+  has_many :cart_items, dependent: :destroy
+  has_many :products, through: :cart_items
 
-  # TODO: lógica para marcar o carrinho como abandonado e remover se abandonado
+  validates :total_price, numericality: { greater_than_or_equal_to: 0 }, allow_nil: true
+
+  def mark_as_abandoned
+    update!(abandoned: true)
+  end
+
+  def remove_if_abandoned
+    destroy! if abandoned?
+  end
+
+  def recalculate_total
+    update!(total_price: cart_items.sum(&:total_price), last_interaction_at: Time.current)
+  end
 end
